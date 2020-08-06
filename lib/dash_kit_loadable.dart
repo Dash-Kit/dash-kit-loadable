9b3799d82a90fe2f1a76bca83af0a910ec2f2944
Artem Abramov <artem.abramov@dualbootpartners.com>
2020-08-06 17:42:42 +0300

    Fixed review issues

--- lib/dash_kit_loadable.dart
+++ lib/dash_kit_loadable.dart
@@ -1,4 +1,4 @@
 export './src/blocked_loadable_view.dart';
-export './src/loadableView.dart';
+export './src/loadable_view.dart';
 export './src/loadable_list_view.dart';
 export './src/loadable_paginated_list_view.dart';
